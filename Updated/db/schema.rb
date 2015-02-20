64d26432fdd21c34e846e16d7cc0cd145c695eea
rawan_abdelhalim <rawan.abdelhalim2@gmail.com>
2015-02-20 16:15:15 +0000

    Admin requirement

--- Updated/db/schema.rb
+++ Updated/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150220104109) do
+ActiveRecord::Schema.define(version: 20150220134903) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20150220104109) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
