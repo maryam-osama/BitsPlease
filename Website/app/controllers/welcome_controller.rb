0740f6cee36622e5df1cee1cf72fa23e17e2e3cd
Maryam Osama <maryam.osama06@gmail.com>
2015-02-22 20:22:57 +0200

    Latest Version

---
+++ Website/app/controllers/welcome_controller.rb
@@ -0,0 +1,30 @@
+class WelcomeController < ApplicationController
+   def index
+   end
+  
+   def norineprof
+   end
+   
+   def basmaprof
+   end
+   
+   def khadija
+   end
+   
+   def maryam
+   end
+   
+   def mai
+   end
+   
+   def sama
+   end
+   
+   def rawan
+   end
+
+   def fady
+   end
+  
+  
+end
